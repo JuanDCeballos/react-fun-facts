4f3f8ad9a0fe7c872bfcca2d3bbfc29dd676236e
Juan Ceballos <juandavi789@hotmail.com>
2023-08-24 08:03:00 -0500

    Update file for practice

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,40 @@
 import reactLogo from './assets/react.svg'
 // import './App.css'
 
-function App() {
+// const App = () => {
+//   return (
+//     <>
+//       <div>
+//         <img src={reactLogo} />
+//         <h1>Fun facts about React</h1>
+//         <ul>
+//           <li>Was first released in 2013</li>
+//           <li>Was originally created by Jordan Walke</li>
+//           <li>Has well over 100K stars on Github</li>
+//           <li>Is maintained by Facebook</li>
+//           <li>Power thousands of enterprise app, including mobile apps</li>
+//         </ul>
+//       </div>
+//     </>
+//   )
+// }
 
+const App = () => {
   return (
     <>
-      <div>
-        <img src={reactLogo} />
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Power thousands of enterprise app, including mobile apps</li>
-        </ul>
-      </div>
+    <header>
+      <nav>
+        <img src={reactLogo}/>
+      </nav>
+    </header>
+    <h1>Why I&apos;m learning React?</h1>
+    <ol>
+      <li>It&apos;s fun</li>
+      <li>It&apos;s beautiful</li>
+      <li>It&apos;s high demanded on the market</li>
+      <li>You can develop amazing this with this</li>
+    </ol>
+    <footer>© 2023 Ceballos development. All rights reserved.</footer>
     </>
   )
 }
