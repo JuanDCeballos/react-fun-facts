927115ef5262c256c4db62aadb32019a76cd7ddf
Juan Ceballos <juandavi789@hotmail.com>
2023-08-23 20:15:08 -0500

    Initial setup and structure of the app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+// import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} />
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Power thousands of enterprise app, including mobile apps</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
