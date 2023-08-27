947d351da51f38d7e8a6d63630339eed5c99f043
Juan Ceballos <juandavi789@hotmail.com>
2023-08-27 01:59:54 -0500

    Update structure of the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 // import viteLogo from '/vite.svg'
 import './App.css';
-import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-import Header from './components/Header';
-
-// const App = () => {
-//   return (
-//     <>
-//       <div>
-//         <img src={reactLogo} />
-//         <h1>Fun facts about React</h1>
-//         <ul>
-//           <li>Was first released in 2013</li>
-//           <li>Was originally created by Jordan Walke</li>
-//           <li>Has well over 100K stars on Github</li>
-//           <li>Is maintained by Facebook</li>
-//           <li>Power thousands of enterprise app, including mobile apps</li>
-//         </ul>
-//       </div>
-//     </>
-//   )
-// }
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='main-container'>
       <div className='card'>
-        <Header />
+        <Navbar />
         <MainContent />
-        <Footer />
       </div>
     </div>
   );
