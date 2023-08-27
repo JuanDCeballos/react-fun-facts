2eb5f59cdac0e7099f676c8700b95f116e7b2224
Juan Ceballos <juandavi789@hotmail.com>
2023-08-27 02:00:49 -0500

    Add new component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import reactLogo from '../assets/react.svg';
+
+const Navbar = () => {
+  return (
+    <nav className='nav-container'>
+      <img src={reactLogo} />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+};
+
+export default Navbar;
